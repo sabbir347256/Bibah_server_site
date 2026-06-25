57ac711f00f8f6cd3aa975804f1965a8d570591a
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-06-25 17:50:43 +0600

    some function update

--- src/modules/Request/request.routes.ts
+++ src/modules/Request/request.routes.ts
@@ -8,7 +8,7 @@ const router = Router();
 
 
 router.post('/send',checkAuth(Role?.USER), requestController.sendConnectionRequest);
-router.post('/action', checkAuth(Role?.USER), requestController.handleRequestAction);
+router.post('/action', checkAuth(Role?.USER),requestController.handleRequestAction);
 router.get('/pending',checkAuth(Role?.USER), requestController.getPendingRequests);
 router.get('/status/:targetUserId',checkAuth(Role?.USER), requestController.getConnectionStatus);
 
